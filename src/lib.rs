7eca63a50d94c1977b59428ab5c4ef52915edd2c
Dmitrii - Demenev <demenev.dmitriy1@mail.ru>
2022-02-02 20:25:40 -0700

    Tweaked implementation. Hopefully will work

--- src/lib.rs
+++ src/lib.rs
@@ -9,14 +9,21 @@ use proc_macro::TokenStream;
 use proc_macro2::TokenTree as TokenTree2;
 use quote::quote;
 
-macro_rules! get_is_signed_and_max_len {
+macro_rules! get_is_signed_and_max_len_wo_sign {
     ($type_name:ident in @PRIM_INTS) => {
-        get_is_signed_and_max_len!($type_name in [u8,u16,u32,u64,u128,usize,i8,i16,i32,i64,i128,isize])
+        get_is_signed_and_max_len_wo_sign!($type_name in [u8,u16,u32,u64,u128,usize,i8,i16,i32,i64,i128,isize])
     };
     ($type_name:ident in [$($t:ty),+]) => {
         match $type_name.as_str() {
             $(
-                stringify!($t) => (<$t>::IS_SIGNED, <$t>::MAX_LEN_BASE_10_AS_USIZE),
+                stringify!($t) => (
+                    <$t>::IS_SIGNED,
+                    if <$t>::IS_SIGNED {
+                        <$t>::MAX_LEN_BASE_10_AS_USIZE - 1 
+                    } else {
+                        <$t>::MAX_LEN_BASE_10_AS_USIZE
+                    }
+                ),
             )+
             _ => panic!("unexpected type"),
         }
@@ -73,13 +80,13 @@ fn get_implementation_for_signed(type_token: &TokenTree2,
     smallest_numbers_with_corresponding_lengths: &Vec<usize>
 ) -> TokenStream {
     quote! {
-        impl const GetLenBase10AsUsizeViaDivigingWithPowsOf2 {
+        impl const GetLenBase10AsUsizeViaDivigingWithPowsOf2 for #type_token {
             fn get_len_base_10_as_usize_via_dividing_with_pows_of_2(&self) -> usize {
                 let number: Self = *Self;
                 let mut length: usize = if number < 0 { 2 } else { 1 };
                 let mut neg_abs = number.wrapping_abs().wrapping_neg();
                 #(
-                    if (neg_abs >= (#smallest_numbers_with_corresponding_lengths as #type_token)) {
+                    if (neg_abs <= (#smallest_numbers_with_corresponding_lengths as #type_token).wrapping_neg()) {
                         length += #lengths;
                         neg_abs /= (#smallest_numbers_with_corresponding_lengths as #type_token);
                     }
@@ -96,13 +103,13 @@ fn get_implementation_for_signed(type_token: &TokenTree2,
     smallest_numbers_with_corresponding_lengths: &Vec<usize>
 ) -> TokenStream {
     quote! {
-        impl GetLenBase10AsUsizeViaDivigingWithPowsOf2 {
-            fn get_len_base_10_as_usize_via_dividing_with_pows_of_2(&self) {
+        impl GetLenBase10AsUsizeViaDivigingWithPowsOf2 for #type_token {
+            fn get_len_base_10_as_usize_via_dividing_with_pows_of_2(&self) -> usize {
                 let number: Self = *Self;
                 let mut length: usize = if number < 0 { 2 } else { 1 };
                 let mut neg_abs = number.wrapping_abs().wrapping_neg();
                 #(
-                    if (neg_abs >= (#smallest_numbers_with_corresponding_lengths as #type_token)) {
+                    if (neg_abs <= (#smallest_numbers_with_corresponding_lengths as #type_token).wrapping_neg()) {
                         length += #lengths;
                         neg_abs /= (#smallest_numbers_with_corresponding_lengths as #type_token);
                     }
@@ -119,12 +126,12 @@ fn get_implementation_for_signed(type_token: &TokenTree2,
 pub fn impl_get_len_base_10_as_usize_via_dividing_with_pows_of_2(ts: TokenStream) -> TokenStream {
     let ts = proc_macro2::TokenStream::from(ts);
     let type_name = ts.clone().to_string();
-    let (is_signed, max_len): (bool, usize) = get_is_signed_and_max_len!(type_name in @PRIM_INTS);
+    let (is_signed, max_len_wo_sign): (bool, usize) = get_is_signed_and_max_len_wo_sign!(type_name in @PRIM_INTS);
     let type_token: TokenTree2 = ts.into_iter().next().unwrap().into();
     let max_exponent_of_2: u32 = (1u32..)
         .map(|exponent_of_2| 2usize.pow(exponent_of_2))
         // TODO: verify correctness
-        .take_while(|power_of_2| *power_of_2 < max_len)
+        .take_while(|power_of_2| *power_of_2 < max_len_wo_sign)
         .count() as u32;
     let (lengths, smallest_numbers_with_corresponding_lengths) = (0u32..max_exponent_of_2)
         .rev()
